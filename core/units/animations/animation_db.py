861d2bdeccad90c5ccb68aa15e6af6893d299141
Father Karras <adgonbnet@gmail.com>
2021-03-14 00:45:43 +0100

    Fix

--- core/units/animations/animation_db.py
+++ core/units/animations/animation_db.py
@@ -42,15 +42,6 @@ LizardSet = [
     AnimationResource(AnimationType.MISS, 4)
 ]
 
-DragonSet = [
-    # Index 0: Idle, 1: Death, 2: Attack, 3:Hurt, 4:Block, 5: Miss
-    AnimationResource(AnimationType.IDLE, 8),
-    AnimationResource(AnimationType.DEATH, 10),
-    AnimationResource(AnimationType.ATTACK, 8),
-    AnimationResource(AnimationType.HURT, 3),
-    AnimationResource(AnimationType.BLOCK, 9),
-    AnimationResource(AnimationType.MISS, 5),
-]
 
 DragonSet = [
     # Index 0: Idle, 1: Death, 2: Attack, 3:Hurt, 4:Block, 5: Miss
