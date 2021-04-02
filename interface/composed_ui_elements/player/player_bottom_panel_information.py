59e48f363370d6af97b45052043a67bb83c40ed4
Asigan <asiganthesunk@gmail.com>
2021-04-02 04:33:47 +0200

    Fix font size issue

--- interface/composed_ui_elements/player/player_bottom_panel_information.py
+++ interface/composed_ui_elements/player/player_bottom_panel_information.py
@@ -4,6 +4,7 @@
 from interface.ui_elements.ui_layout import UILayout
 from interface.ui_elements.ui_text_element import UITextElement
 from interface.constants.player_messages import PLAYER_HP, PLAYER_MP, PLAYER_FURY, PLAYER_CONSUMABLE
+from constants.game_fonts import default_font
 
 
 class PlayerBottomPanelInformation(UILayout):
@@ -24,8 +25,10 @@ class PlayerBottomPanelInformation(UILayout):
         self.add(UITextElement(PLAYER_HP(player.current_hp, player.max_hp), (440, self.panel_height_correction + 18)))
         self.add(UITextElement(PLAYER_MP(player.current_mp, player.max_mp), (440, self.panel_height_correction + 38)))
         self.add(UITextElement(PLAYER_FURY(player.current_fury, player.max_fury), (440, self.panel_height_correction + 58)))
-        self.add(UITextElement(PLAYER_CONSUMABLE(player.stash.healing_potions), (190, self.panel_height_correction + 20)))
-        self.add(UITextElement(PLAYER_CONSUMABLE(player.stash.mana_potions), (190, self.panel_height_correction + 80)))
+        self.add(UITextElement(PLAYER_CONSUMABLE(player.stash.healing_potions),
+                               (190, self.panel_height_correction + 20), font=default_font))
+        self.add(UITextElement(PLAYER_CONSUMABLE(player.stash.mana_potions),
+                               (190, self.panel_height_correction + 80), font=default_font))
 
 
 
